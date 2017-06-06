1f942b1fbf5d1bf8e4482231c75ebe59b22fbfd1
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-06 13:16:53 +0200

    Added webpack with (basic) build process.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js(\.jsx)?$/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015'],
+              cacheDirectory: 'tmp/babel-cache',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'application.js',
+    path: path.join(__dirname, 'build'),
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: '../static' }
+    ])
+  ],
+  resolve: {
+    extensions: ['.js', '.js.jsx'],
+    modules: ['src', 'node_modules'],
+  },
+};
