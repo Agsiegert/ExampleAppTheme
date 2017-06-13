8749dcc21f398a9c3517eb270ddfdf32fe6d0011
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-13 13:24:35 +0200

    Add a desing specific dev server (yarn serve:design)

---
+++ webpack.design.config.js
@@ -0,0 +1,48 @@
+const process = require('process');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const env = process.env.NODE_ENV || 'production';
+const isDevelopment = env === 'development';
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    'index.css': './stylesheets/index.scss',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: !isDevelopment,
+              },
+            }, {
+              loader: 'sass-loader',
+            },
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: '../design' },
+      { from: '../vendor/scrivito', to: 'scrivito' },
+    ]),
+    new ExtractTextPlugin({
+      filename: '[name]',
+    }),
+  ],
+  resolve: {
+    modules: ['node_modules'],
+  },
+  devServer: {
+    port: 4000,
+  },
+};
