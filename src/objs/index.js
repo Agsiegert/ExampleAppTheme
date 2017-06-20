c8131e0384e8acfc34fd4f1f477e21761ab6a322
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-20 12:49:33 +0000

    Added a dedicated "Homepage" class.
    This will be uses for "Obj.root" and will contain all footers etc.

--- src/objs/index.js
+++ src/objs/index.js
@@ -4,5 +4,8 @@ window.ExampleApp.Blog = Blog;
 import BlogPost from './blog_post';
 window.ExampleApp.BlogPost = BlogPost;
 
+import Homepage from './homepage';
+window.ExampleApp.Homepage = Homepage;
+
 import Page from './page';
 window.ExampleApp.Page = Page;
