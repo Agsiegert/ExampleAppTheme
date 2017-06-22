2d755e3d2b18bb81430871b629bdfd84876024c7
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-22 10:02:52 +0000

    Added DangerZone, which can reset all contents.
    ImportContent is empty for now.

---
+++ src/danger_zone/index.js
@@ -0,0 +1,7 @@
+window.ExampleApp.DangerZone = {};
+
+import reset from './reset';
+window.ExampleApp.DangerZone.reset = reset;
+
+import importContent from './import_content';
+window.ExampleApp.DangerZone.importContent = importContent;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import 'objs/index';
 import 'widgets/index';
 import App from 'app';
 import 'config/scrivito';
+import 'danger_zone/index';
 
 ReactDOM.render(<App />, document.getElementById('application'));
