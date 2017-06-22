e7dccc29d5587336014b1a08ed429cc5f988c86e
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-22 14:57:36 +0200

    [content] Ordered children for navigation

--- src/danger_zone/import_content.js
+++ src/danger_zone/import_content.js
@@ -1,50 +1,54 @@
 export default () => {
-  window.ExampleApp.Homepage.create({
-    _path: '/',
-    title: 'Welcome to the Scrivito Example App JS!',
-  });
-
   // HOME VARIANTS
-  window.ExampleApp.Page.create({
-    _path: '/home_2',
-    title: 'Homepage variant 2',
-  });
-  window.ExampleApp.Page.create({
+  const home3 = window.ExampleApp.Page.create({
     _path: '/home_2/home_3',
     title: 'Homepage variant 3',
   });
+  const home = window.ExampleApp.Page.create({
+    _path: '/home_2',
+    title: 'Homepage variant 2',
+    childOrder: [home3],
+  });
 
   // PRODUCT
-  window.ExampleApp.Page.create({
+  const product = window.ExampleApp.Page.create({
     _path: '/product',
     title: 'Product',
   });
 
   // ABOUT
-  window.ExampleApp.Page.create({
-    _path: '/about',
-    _permalink: 'about',
-    title: 'About',
-  });
-  window.ExampleApp.Page.create({
+  const ourWork = window.ExampleApp.Page.create({
     _path: '/about/our_work',
     title: 'Our Work',
   });
-  window.ExampleApp.Page.create({
+  const ourClients = window.ExampleApp.Page.create({
     _path: '/about/our_clients',
     title: 'Our Clients',
   });
+  const about = window.ExampleApp.Page.create({
+    _path: '/about',
+    _permalink: 'about',
+    title: 'About',
+    childOrder: [ourWork, ourClients],
+  });
 
   // BLOG
-  window.ExampleApp.Blog.create({
+  const blog = window.ExampleApp.Blog.create({
     _path: '/blog',
     _permalink: 'blog',
     title: 'Blog',
   });
 
   // ALL WIDGETS
-  window.ExampleApp.Blog.create({
+  const allWidgets = window.ExampleApp.Blog.create({
     _path: '/all_widgets',
     title: 'All Widgets',
+  });
+
+  // Obj.root
+  window.ExampleApp.Homepage.create({
+    _path: '/',
+    title: 'Welcome to the Scrivito Example App JS!',
+    childOrder: [home, product, about, blog, allWidgets],
   });
 };
