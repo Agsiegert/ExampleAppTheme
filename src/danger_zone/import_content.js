e407b1145b802c405f337558214230eb742f950e
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-29 10:27:09 +0000

    [content] Use real imports.

--- src/danger_zone/import_content.js
+++ src/danger_zone/import_content.js
@@ -1,13 +1,17 @@
 /* eslint no-console: 0 */
+import Blog from 'objs/blog';
+import Homepage from 'objs/homepage';
+import Image from 'objs/image';
+import Page from 'objs/page';
 import logoSvgData from './logo_svg_data';
 
 export default () => {
   // HOME VARIANTS
-  const home3 = window.ExampleApp.Page.create({
+  const home3 = Page.create({
     _path: '/home_2/home_3',
     title: 'Homepage variant 3',
   });
-  const home = window.ExampleApp.Page.create({
+  const home = Page.create({
     _path: '/home_2',
     title: 'Homepage variant 2',
     navigationTitle: 'Home',
@@ -15,21 +19,21 @@ export default () => {
   });
 
   // PRODUCT
-  const product = window.ExampleApp.Page.create({
+  const product = Page.create({
     _path: '/product',
     title: 'Product',
   });
 
   // ABOUT
-  const ourWork = window.ExampleApp.Page.create({
+  const ourWork = Page.create({
     _path: '/about/our_work',
     title: 'Our Work',
   });
-  const ourClients = window.ExampleApp.Page.create({
+  const ourClients = Page.create({
     _path: '/about/our_clients',
     title: 'Our Clients',
   });
-  const about = window.ExampleApp.Page.create({
+  const about = Page.create({
     _path: '/about',
     _permalink: 'about',
     title: 'About',
@@ -37,31 +41,29 @@ export default () => {
   });
 
   // BLOG
-  const blog = window.ExampleApp.Blog.create({
+  const blog = Blog.create({
     _path: '/blog',
     _permalink: 'blog',
     title: 'Blog',
   });
 
   // ALL WIDGETS
-  const allWidgets = window.ExampleApp.Page.create({
+  const allWidgets = Page.create({
     _path: '/all_widgets',
     title: 'All Widgets',
   });
 
   // navigation logo
   const svgBlob = new Blob([logoSvgData], { type: 'image/svg+xml' });
-  const binary = window.Scrivito.Binary.upload(svgBlob, { filename: 'scrivito_logo_sw.svg' });
-  const logo = window.ExampleApp.Image.create({
-    title: 'Scrivito Logo SW',
-  });
+  const binary = Scrivito.Binary.upload(svgBlob, { filename: 'scrivito_logo_sw.svg' });
+  const logo = Image.create({ title: 'Scrivito Logo SW' });
   binary.into(logo).then(newBinary => {
     logo.update({ blob: newBinary });
     console.log('Import done');
   });
 
   // Obj.root
-  window.ExampleApp.Homepage.create({
+  Homepage.create({
     _path: '/',
     title: 'Welcome to the Scrivito Example App JS!',
     childOrder: [home, product, about, blog, allWidgets],
