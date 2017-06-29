6604aca5399133bfd226ddffab3556d53e757129
Alexander Pepper <alexander.pepper@infopark.de>
2017-06-29 10:27:40 +0000

    Remove unneeded globals.
    They can be later accessed via "Scrivito.getClass('Blog')".

--- src/objs/index.js
+++ src/objs/index.js
@@ -1,11 +1,7 @@
+/* eslint no-unused-vars: "off" */
+
 import Blog from './blog';
 import BlogPost from './blog_post';
 import Homepage from './homepage';
 import Image from './image';
 import Page from './page';
-
-window.ExampleApp.Blog = Blog;
-window.ExampleApp.BlogPost = BlogPost;
-window.ExampleApp.Homepage = Homepage;
-window.ExampleApp.Image = Image;
-window.ExampleApp.Page = Page;
--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,5 +1,4 @@
+/* eslint no-unused-vars: "off" */
+
 import HeadlineWidget from './headline_widget';
 import TextWidget from './text_widget';
-
-window.ExampleApp.HeadlineWidget = HeadlineWidget;
-window.ExampleApp.TextWidget = TextWidget;
