2b3bb0789275298a6aacc7db6bea54393b1e8b10
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-11 09:20:22 +0000

    Extracted FontAwesomeIconComponent.
    So it can be rendered without widget markers.

---
+++ src/components/font_awesome_icon.js.jsx
@@ -0,0 +1,23 @@
+const FontAwesomeIconPlainComponent = {
+  render() {
+    const widget = this.props.widget;
+    const icon = widget.get('icon') || 'fa-coffee';
+    const link = widget.get('link');
+    if (!link) {
+      return <i className= { ['fa', icon].join(' ') } aria-hidden="true"></i>;
+    }
+
+    const title = link.title || '';
+
+    return (
+      <Scrivito.React.Link to={ link }>
+        <i className= { ['fa', icon].join(' ') } aria-hidden="true" title={ title }></i>
+      </Scrivito.React.Link>
+    );
+  },
+};
+
+const FontAwesomeIconComponent = Scrivito.createComponent(FontAwesomeIconPlainComponent);
+
+export { FontAwesomeIconPlainComponent };
+export default FontAwesomeIconComponent;
--- src/widgets/font_awesome_icon_widget.js.jsx
+++ src/widgets/font_awesome_icon_widget.js.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIconPlainComponent } from 'components/font_awesome_icon';
+
 const FontAwesomeIconWidget = Scrivito.createWidgetClass({
   name: 'FontAwesomeIconWidget',
   attributes: {
@@ -21,21 +23,6 @@ Scrivito.provideUiConfig(FontAwesomeIconWidget, {
   },
 });
 
-Scrivito.provideComponent(FontAwesomeIconWidget, {
-  render() {
-    const widget = this.props.widget;
-    const icon = widget.get('icon') || 'fa-coffee';
-    const link = widget.get('link');
-    if (!link) {
-      return <i className= { ['fa', icon].join(' ') } aria-hidden="true"></i>;
-    }
-
-    const title = link.title || '';
-
-    return <Scrivito.React.Link to={ link }>
-      <i className= { ['fa', icon].join(' ') } aria-hidden="true" title={ title }></i>
-    </Scrivito.React.Link>;
-  },
-});
+Scrivito.provideComponent(FontAwesomeIconWidget, FontAwesomeIconPlainComponent);
 
 export default FontAwesomeIconWidget;
