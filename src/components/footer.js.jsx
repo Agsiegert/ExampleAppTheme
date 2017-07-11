9fc2a1c29a1bdbb3d40198aef974a43679da0a36
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-11 11:46:38 +0200

    [footer] Edit social icons only in the homepage details.

--- src/components/footer.js.jsx
+++ src/components/footer.js.jsx
@@ -1,10 +1,21 @@
+import FontAwesomeIconComponent from 'components/font_awesome_icon';
+
 const Footer = Scrivito.createComponent(() => {
   const root = Scrivito.Obj.root();
   return (
     <footer>
       <Scrivito.React.Content content={ root } attribute="footer" />
       <div className="bg-greydark height-center social-footer">
-        <Scrivito.React.Content className="container" content={ root } attribute="socialButtons" />
+        <div className="container">
+          {
+            root.get('socialButtons').map(socialButton =>
+              <FontAwesomeIconComponent
+                  widget={ socialButton }
+                  key= { socialButton.id }
+                />
+            )
+          }
+        </div>
       </div>
     </footer>
   );
