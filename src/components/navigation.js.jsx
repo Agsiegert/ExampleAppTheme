69ce1aeae8acff9589cace823f4e86611fda8ac0
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-12 11:04:10 +0200

    First (basic) navigation.
    * Mobile not done yet.
    * Scolling not done yet.
    * No logo yet.
    * No search icon yet.

--- src/components/navigation.js.jsx
+++ src/components/navigation.js.jsx
@@ -1,71 +1,54 @@
-import { NavDropdown, MenuItem } from 'react-bootstrap';
-
 const Navigation = Scrivito.createComponent(() => {
   return (
-    <header>
-      <Scrivito.React.Link to={ Scrivito.Obj.root() } className="logo">
-        <img src={ Scrivito.Obj.root().get('logo').get('blob').url } />
-      </Scrivito.React.Link>
-      <Scrivito.React.ChildList
-        className="nav nav-pills"
-        parent={ Scrivito.Obj.root() }
-        renderChild={ renderChild }
-      />
-    </header>
+    <section className="bg-white">
+      <nav className="navbar navbar-fixed-top">
+        <div className="container">
+          <div id="navbar" className="collapse navbar-collapse">
+            <Scrivito.React.ChildList
+              className="nav navbar-nav navbar-right"
+              parent={ Scrivito.Obj.root() }
+              renderChild={ renderChild }
+            />
+          </div>
+        </div>
+      </nav>
+    </section>
   );
 });
 
-function uppercaseTitle(child) {
-  return child.get('title').toUpperCase();
-}
-
 function renderChild(child) {
-  if (child.children.length > 0) {
-    return <ChildrenDropdown page= { child } />;
+  if (child.children.length === 0) {
+    return renderSingleChild(child);
   }
 
-  return <li className={ isActiveSection(child) && 'active' }>
+  return (
+    <li className="dropdown">
+      <Scrivito.React.Link
+          to={ child }
+          className="dropdown-toggle"
+          role="button"
+          aria-haspopup="true"
+          aria-expanded="false">
+        { child.get('title') }<span className="caret"></span>
+      </Scrivito.React.Link>
+      <span className="mobile-toggle">
+        <i className="fa fa-angle-down" aria-hidden="true"></i>
+      </span>
+      <Scrivito.React.ChildList
+        className="dropdown-menu"
+        parent={ child }
+        renderChild={ renderSingleChild }
+      />
+    </li>
+  );
+}
+
+function renderSingleChild(child) {
+  return (<li>
     <Scrivito.React.Link to={ child }>
-      { uppercaseTitle(child) }
+      { child.get('title') }
     </Scrivito.React.Link>
-  </li>;
+  </li>);
 }
-
-function isActiveSection(page) {
-  if (!Scrivito.currentPage()) { return false; }
-
-  const currentPath = Scrivito.currentPage().path;
-  if (!currentPath || currentPath === '/') { return false; }
-
-  return currentPath.startsWith(page.path);
-}
-
-function isActive(page) {
-  if (!Scrivito.currentPage()) { return false; }
-
-  return Scrivito.currentPage().id === page.id;
-}
-
-const ChildrenDropdown = Scrivito.createComponent(({ page }) => {
-  return <NavDropdown
-      title={ uppercaseTitle(page) }
-      id="nav-dropdown"
-      className={ isActiveSection(page) && 'active' }>
-    <DropdownItem page={ page } key={ page.id } />
-    {
-      page.children.map(grandchild => {
-        return <DropdownItem page={ grandchild } key={ grandchild.id } />;
-      })
-    }
-  </NavDropdown>;
-});
-
-const DropdownItem = Scrivito.createComponent(({ page, ...props }) => {
-  const onClick = () => Scrivito.navigateTo(() => page);
-
-  return <MenuItem { ...props } onClick={ onClick } className={ isActive(page) && 'active' }>
-    { page.get('title') }
-  </MenuItem>;
-});
 
 export default Navigation;
