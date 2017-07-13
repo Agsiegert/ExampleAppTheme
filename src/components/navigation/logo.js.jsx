4dd57018a3c6af353e9f8575b980bc04ab187781
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-13 13:54:04 +0200

    Logo is now rendered using inline css.

--- src/components/navigation/logo.js.jsx
+++ src/components/navigation/logo.js.jsx
@@ -1,8 +1,16 @@
+function logoStyle() {
+  const logoUrl = Scrivito.Obj.root().get('logo').get('blob').url;
+
+  return {
+    background: `rgba(0, 0, 0, 0) url(${logoUrl}) no-repeat scroll center center / contain`,
+  };
+}
+
 const Logo = Scrivito.createComponent(() =>
   <Scrivito.React.Link
       to={ Scrivito.Obj.root() }
-      className="navbar-brand">
-    <img src={ Scrivito.Obj.root().get('logo').get('blob').url } />
+      className="navbar-brand"
+      style={ logoStyle() }>
   </Scrivito.React.Link>
 );
 
