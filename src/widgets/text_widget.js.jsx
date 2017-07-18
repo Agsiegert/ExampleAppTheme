b5676e17215ee2639a920009a278d415137f6eba
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-18 12:11:54 +0200

    TextWidget can now be centered.

--- src/widgets/text_widget.js.jsx
+++ src/widgets/text_widget.js.jsx
@@ -2,16 +2,28 @@ const TextWidget = Scrivito.createWidgetClass({
   name: 'TextWidget',
   attributes: {
     text: 'html',
+    centered: ['enum', { validValues: ['yes', 'no'] }],
   },
 });
 
 Scrivito.provideUiConfig(TextWidget, {
   title: 'Text',
   description: 'A widget with html text.',
+  attributes: {
+    centered: {
+      title: 'Centered',
+      description: 'Should this text be centered?',
+    },
+  },
 });
 
 Scrivito.provideComponent(TextWidget, widget =>
-  <Scrivito.React.Content tag="div" content={ widget } attribute="text" />
+  <Scrivito.React.Content
+    tag="div"
+    className={ widget.get('centered') === 'yes' && 'text-center' }
+    content={ widget }
+    attribute="text"
+    />
 );
 
 export default TextWidget;
