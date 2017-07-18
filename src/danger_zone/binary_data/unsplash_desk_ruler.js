31e2b12b7b6e0820c1d0130978f2cd7053fb5219
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-18 11:07:53 +0200

    [content] "Our top feature" section.
    Also added tags to unspash images.

---
+++ src/danger_zone/binary_data/unsplash_desk_ruler.js
@@ -0,0 +1,6 @@
+/* eslint linebreak-style: 0 */
+
+export default {
+  filename: 'jeff-sheldon-3231.jpg',
+  url: 'http://unsplash.com/photos/JWiMShWiF14/download?force=true',
+};
---
+++ src/danger_zone/binary_data/unsplash_laptop_keyboard.js
@@ -0,0 +1,6 @@
+/* eslint linebreak-style: 0 */
+
+export default {
+  filename: 'glenn-carstens-peters-203007.jpg',
+  url: 'http://unsplash.com/photos/npxXWgQ33ZQ/download?force=true',
+};
