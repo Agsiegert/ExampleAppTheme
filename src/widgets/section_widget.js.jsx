6abf1b59a70f397c4a72d8a12c813a737cce93bf
Alexander Pepper <alexander.pepper@infopark.de>
2017-07-19 09:24:06 +0200

    SectionWidget can use full screen width.

--- src/widgets/section_widget.js.jsx
+++ src/widgets/section_widget.js.jsx
@@ -2,16 +2,30 @@ const SectionWidget = Scrivito.createWidgetClass({
   name: 'SectionWidget',
   attributes: {
     content: 'widgetlist',
+    useFullWidth: ['enum', { validValues: ['yes', 'no'] }],
   },
 });
 
 Scrivito.provideUiConfig(SectionWidget, {
   title: 'Section',
   description: 'A section',
+  attributes: {
+    useFullWidth: {
+      title: 'Use full width?',
+      description: 'Should this section use the full screen width?',
+    },
+  },
 });
 
-Scrivito.provideComponent(SectionWidget, widget => <section className='bg-white'>
-    <Scrivito.React.Content className="container" content={ widget } attribute="content" />
+Scrivito.provideComponent(SectionWidget, widget => {
+  let className = 'container';
+  if (widget.get('useFullWidth') === 'yes') {
+    className = 'container-fluid gutter0';
+  }
+
+  return (<section className='bg-white'>
+    <Scrivito.React.Content className={ className } content={ widget } attribute="content" />
   </section>);
+});
 
 export default SectionWidget;
