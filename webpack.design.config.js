7a47740411508206ebb51d63b55aea01c99d5e52
Alexander Pepper <alexander.pepper@infopark.de>
2017-08-02 12:10:27 +0000

    [design] Use assets of slick slider.

--- webpack.design.config.js
+++ webpack.design.config.js
@@ -29,7 +29,7 @@ module.exports = {
         }),
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        test: /\.(eot|svg|ttf|woff|woff2|gif)$/,
         use: [
           'file-loader',
         ],
