57940b0b7d55a2c85b48ea665000237b418ab14f
Alexander Pepper <alexander.pepper@infopark.de>
2017-08-07 12:17:46 +0200

    Added basic social card

--- src/components/scrivito_extensions/social_cards_tab.js.jsx
+++ src/components/scrivito_extensions/social_cards_tab.js.jsx
@@ -1,3 +1,10 @@
-Scrivito.registerComponent('SocialCardsTab', content =>
-  <div>Custom tab content for { content.objClass }</div>
+Scrivito.registerComponent('SocialCardsTab', obj =>
+  <div>
+    <h3>Twitter card</h3>
+    <div className="seo_card_preview">
+      <div className="row">
+        <Scrivito.React.Content content={ obj } attribute="tcCreator" className="col-md-12" />
+      </div>
+    </div>
+  </div>
 );
