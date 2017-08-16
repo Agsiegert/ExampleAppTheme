48025f5150f292bd1da5e5fe51d34dfc47581401
Alexander Pepper <alexander.pepper@infopark.de>
2017-08-16 10:09:03 +0200

    BlogPost design improvements

--- src/components/blog_post/blog_post_tag_list.js.jsx
+++ src/components/blog_post/blog_post_tag_list.js.jsx
@@ -1,11 +1,11 @@
 function BlogPostTagList({ tags }) {
   return (
-    <section className="bg-nav-content auto-height">
+    <section>
       <div className="container">
         <div className="nav-centered">
           <ul className="nav nav-pills">
             {
-              tags.map(tag => <li key={ tag } role="presentation"><a href="#">{ tag }</a></li>)
+              tags.map(tag => <li key={ tag }><a href="#">{ tag }</a></li>)
             }
           </ul>
         </div>
