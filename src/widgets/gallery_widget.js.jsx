60a713eacaa8be8443f8639b4643e1caff75ed0c
Alexander Pepper <alexander.pepper@infopark.de>
2017-08-23 10:29:10 +0200

    Extracted ThumbnailGallery from GalleryWidget.

--- src/widgets/gallery_widget.js.jsx
+++ src/widgets/gallery_widget.js.jsx
@@ -1,5 +1,4 @@
-import Lightbox from 'react-images';
-import { GalleryImageWidgetComponent } from 'widgets/gallery_image_widget';
+import ThumbnailGallery from 'widgets/gallery_widget/thumbnail_gallery';
 
 const GalleryWidget = Scrivito.createWidgetClass({
   name: 'GalleryWidget',
@@ -19,86 +18,8 @@ Scrivito.provideUiConfig(GalleryWidget, {
   },
 });
 
-class GalleryWidgetComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentImage: 0,
-      lightboxIsOpen: false,
-    };
-
-    this.openLightbox = this.openLightbox.bind(this);
-    this.closeLightbox = this.closeLightbox.bind(this);
-    this.gotoPrevious = this.gotoPrevious.bind(this);
-    this.gotoNext = this.gotoNext.bind(this);
-    this.gotoImage = this.gotoImage.bind(this);
-  }
-
-  openLightbox(index, event) {
-    event.preventDefault();
-    this.setState({
-      currentImage: index,
-      lightboxIsOpen: true,
-    });
-  }
-
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false,
-    });
-  }
-
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1,
-    });
-  }
-
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1,
-    });
-  }
-
-  gotoImage(index) {
-    this.setState({
-      currentImage: index,
-    });
-  }
-
-  render() {
-    const widget = this.props.widget;
-    const images = widget.get('images');
-    const lightboxImages = images.map(image => image.lightboxOptions());
-
-    return (
-      <div className="row">
-        {
-          images.map((image, imageIndex) =>
-            <GalleryImageWidgetComponent
-              key={ image.id }
-              widget={ image }
-              openLightbox={ event => this.openLightbox(imageIndex, event) }
-            />)
-        }
-        <Lightbox
-          images={ lightboxImages }
-          currentImage={ this.state.currentImage }
-          isOpen={ this.state.lightboxIsOpen }
-          onClickImage={ this.handleClickImage }
-          onClickNext={ this.gotoNext }
-          onClickPrev={ this.gotoPrevious }
-          onClickThumbnail={ this.gotoImage }
-          onClose={ this.closeLightbox }
-          showThumbnails
-          backdropClosesModal
-        />
-      </div>
-    );
-  }
-}
-
-Scrivito.provideComponent(GalleryWidget, GalleryWidgetComponent);
+Scrivito.provideComponent(GalleryWidget, ({ widget }) => {
+  return <ThumbnailGallery widget={ widget } />;
+});
 
 export default GalleryWidget;
