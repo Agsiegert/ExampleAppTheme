d91303fda5271cc0c6d2561a59ee399f578f93a2
Antony Siegert <antony.siegert@infopark.de>
2017-08-24 15:28:49 +0200

    adds social cards to blog posts and Helmet head data

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import CurrentPageTitle from 'components/current_page_title';
 import Footer from 'components/footer';
 import Navigation from 'components/navigation';
 import NotFoundErrorPage from 'components/not_found_error_page';
 import InternalErrorPage from 'components/internal_error_page';
+import CurrentPageMetaData from 'components/current_page_meta_data';
 
 export default function App() {
   return (
     <div>
       <div className="content-wrapper">
+        <CurrentPageMetaData />
         <Navigation />
         <Scrivito.React.CurrentPage />
         <NotFoundErrorPage />
         <InternalErrorPage />
       </div>
       <Footer />
-      <CurrentPageTitle />
     </div>
   );
 }
