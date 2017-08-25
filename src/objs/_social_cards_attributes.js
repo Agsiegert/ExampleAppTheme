9d486cf2c9907fd6baf6c257907d9aabd45596b0
Antony Siegert <antony.siegert@infopark.de>
2017-08-25 17:19:12 +0200

    includes PR review comment fixes

--- src/objs/_social_cards_attributes.js
+++ src/objs/_social_cards_attributes.js
@@ -1,17 +1,14 @@
 const socialCardsAttributes = {
   // Twitter Attributes
-  tcCard: 'string', // default to 'summary_large_image'
   tcCreator: 'string', // fallback to author.name
   tcDescription: 'string', // fallback to item.textExtract()
   tcImage: 'reference',  // fallback to titleImage
-  tcSite: 'string', // e.g. @Scrivito
   tcTitle: 'string', // fallback to title
 
   // Open Graph attributes
   ogDescription: 'string', // fallback to item.textExtract()
   ogImage: 'reference', // fallback to titleImage
   ogTitle: 'string', // fallback to title
-  ogType: 'string', // default to 'article'
 };
 
 const socialCardsCustomGroup = {
