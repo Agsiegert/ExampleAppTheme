3a2201d6c4ae38e24196af334028f99b553753c9
Markus Waas <markus.waas@infopark.de>
2017-09-01 10:21:47 +0200

    [content] Three images for client project

---
+++ src/danger_zone/binary_data/unsplash_eureka_tower_night.js
@@ -0,0 +1,6 @@
+/* eslint linebreak-style: 0 */
+
+export default {
+  filename: 'arnaud-mesureur-88396.jpg',
+  url: 'https://unsplash.com/photos/GH7g8Ve-Ubw/download?force=true',
+};
---
+++ src/danger_zone/binary_data/unsplash_skyscraper_night.js
@@ -0,0 +1,6 @@
+/* eslint linebreak-style: 0 */
+
+export default {
+  filename: 'rikki-chan-122581.jpg',
+  url: 'https://unsplash.com/photos/DqKfhGbI-sg/download?force=true',
+};
