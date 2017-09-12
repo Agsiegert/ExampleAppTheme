e2bc1b4a1b97972a119679e98aced240d629619b
Alexander Pepper <alexander.pepper@infopark.de>
2017-09-12 14:24:25 +0200

    Improved JobPreviewWidget.

--- src/utils/full_width_transformed_url.js
+++ src/utils/full_width_transformed_url.js
@@ -9,6 +9,8 @@ function fullScreenWidthPixels() {
 }
 
 function fullWidthTransformedUrl(obj) {
+  if (!obj) { return ''; }
+
   const binary = obj.get('blob');
   // The binary service never scales up, so we transform all images, regardless of their width.
   const transformedBinary = binary.transform({ width: fullScreenWidthPixels() });
--- src/widgets/job_preview_widget.js.jsx
+++ src/widgets/job_preview_widget.js.jsx
@@ -4,45 +4,37 @@ const JobPreviewWidget = Scrivito.createWidgetClass({
   name: 'JobPreviewWidget',
 });
 
-function getImageUrl(image) {
-  if (image) { return fullWidthTransformedUrl(image); }
-
-  return '';
-}
-
 Scrivito.provideComponent(JobPreviewWidget, () => {
   const jobs = Scrivito.getClass('Job').all();
 
   return (
     <div className="row">
       {
-        [...jobs].map((job, index) => {
-          return (
-            <div key={ `${job.id}${index}` } className="col-sm-6">
-              <Scrivito.React.Link to={ job } className="box-card">
-                <span
-                    className="box-image"
-                    style={ {
-                      background: 'no-repeat center / cover',
-                      backgroundImage: `url(${getImageUrl(job.get('image'))})`,
-                    } }
-                  />
-                <span className="box-topic arrow-right">
-                  <h3 className="h3">{ job.get('title') }</h3>
-                  <span>
-                    <i
-                        className={ `fa ${job.get('location') ? 'fa-map-marker' : ''} fa-2x` }
-                        aria-hidden="true"
-                        title="location"
-                      />
-                    <span>{ job.get('location') }</span>
-                  </span>
-                  <i className="fa fa-angle-right" aria-hidden="true"></i>
+        [...jobs].map((job, index) =>
+          <div key={ `${job.id}${index}` } className="col-sm-6">
+            <Scrivito.React.Link to={ job } className="box-card">
+              <span
+                  className="box-image"
+                  style={ {
+                    background: 'no-repeat center / cover',
+                    backgroundImage: `url(${fullWidthTransformedUrl(job.get('image'))})`,
+                  } }
+                />
+              <span className="box-topic arrow-right">
+                <h3 className="h3">{ job.get('title') }</h3>
+                <span>
+                  <i
+                      className={ `fa ${job.get('location') ? 'fa-map-marker' : ''} fa-2x` }
+                      aria-hidden="true"
+                      title="location"
+                    />
+                  <span>{ job.get('location') }</span>
                 </span>
-              </Scrivito.React.Link>
-            </div>
-          );
-        })
+                <i className="fa fa-angle-right" aria-hidden="true"></i>
+              </span>
+            </Scrivito.React.Link>
+          </div>
+        )
       }
     </div>
   );
