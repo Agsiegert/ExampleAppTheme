ab74bbe36e0b234b4ee3ba712034a4a83f65f908
Alexander Pepper <alexander.pepper@infopark.de>
2017-09-21 10:38:18 +0200

    Added ContactFormWidget.

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -4,6 +4,7 @@ import BlogOverviewWidget from './blog_overview_widget';
 import ButtonWidget from './button_widget';
 import CarouselWidget from './carousel_widget';
 import ColumnWidget from './column_widget';
+import ContactFormWidget from './contact_form_widget';
 import DividerWidget from './divider_widget';
 import EventOverviewWidget from './event_overview_widget';
 import FactWidget from './fact_widget';
