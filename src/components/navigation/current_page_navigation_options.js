c20f025b608590dad66fd847808191efb1a9f841
Alexander Pepper <alexander.pepper@infopark.de>
2017-09-22 11:37:20 +0200

    Refactored currentPageNavigationOptions

--- src/components/navigation/current_page_navigation_options.js
+++ src/components/navigation/current_page_navigation_options.js
@@ -1,18 +1,12 @@
 function currentPageNavigationOptions() {
   if (Scrivito.currentPage()) {
     switch (Scrivito.currentPage().objClass()) {
-      case 'Homepage':
-        return pageNavigationOptions(Scrivito.currentPage());
-      case 'Page':
-        return pageNavigationOptions(Scrivito.currentPage());
-      case 'Blog':
-        return blogNavigationOptions(Scrivito.currentPage());
-      case 'BlogPost':
-        return blogPostNavigationOptions(Scrivito.currentPage());
-      case 'Event':
-        return eventNavigationOptions(Scrivito.currentPage());
-      case 'Job':
-        return jobNavigationOptions(Scrivito.currentPage());
+      case 'Blog': return blogNavigationOptions(Scrivito.currentPage());
+      case 'BlogPost': return blogPostNavigationOptions(Scrivito.currentPage());
+      case 'Event': return eventNavigationOptions(Scrivito.currentPage());
+      case 'Homepage': return pageNavigationOptions(Scrivito.currentPage());
+      case 'Job': return jobNavigationOptions(Scrivito.currentPage());
+      case 'Page': return pageNavigationOptions(Scrivito.currentPage());
     }
   }
 
@@ -21,26 +15,6 @@ function currentPageNavigationOptions() {
     backgroundImage: null,
     heigthClassName: null,
     useGradient: false,
-  };
-}
-
-function pageNavigationOptions(obj) {
-  const navStyle = obj.get('navigationStyle') || 'solidWhite';
-
-  let navigationStyle = navStyle;
-  if (navStyle.startsWith('transparentDark')) { navigationStyle = 'transparentDark'; }
-
-  const heigthClassName = navStyle.split(' ')[1] || 'full-height';
-
-  const backgroundImage = obj.get('navigationBackgroundImage') || null;
-
-  const useGradient = navStyle.includes('gradient');
-
-  return {
-    navigationStyle,
-    backgroundImage,
-    heigthClassName,
-    useGradient,
   };
 }
 
@@ -97,6 +71,26 @@ function jobNavigationOptions(obj) {
     navigationStyle: 'solidWhite',
     backgroundImage: null,
     heigthClassName: null,
+  };
+}
+
+function pageNavigationOptions(obj) {
+  const navStyle = obj.get('navigationStyle') || 'solidWhite';
+
+  let navigationStyle = navStyle;
+  if (navStyle.startsWith('transparentDark')) { navigationStyle = 'transparentDark'; }
+
+  const heigthClassName = navStyle.split(' ')[1] || 'full-height';
+
+  const backgroundImage = obj.get('navigationBackgroundImage') || null;
+
+  const useGradient = navStyle.includes('gradient');
+
+  return {
+    navigationStyle,
+    backgroundImage,
+    heigthClassName,
+    useGradient,
   };
 }
 
