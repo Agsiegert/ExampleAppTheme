f59116582165442233dfa194e8cf23d9c63c7509
David Császár <dcsaszar@users.noreply.github.com>
2017-09-26 11:15:55 +0200

    Remove platform specific ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,93 @@
-const process = require('process');
 const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const env = process.env.NODE_ENV || 'production';
-const isDevelopment = env === 'development';
-
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: {
-    index: './index.js',
-    scrivito_extensions: './scrivito_extensions.js',
-    'index.css': './assets/stylesheets/index.scss',
-    danger_zone: './danger_zone.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js(\.jsx)?$/,
-        include: path.join(__dirname, 'src'),
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['react', 'es2015', 'es2016', 'es2017'],
-              plugins: ['transform-object-rest-spread', 'array-includes'],
-              cacheDirectory: 'tmp/babel-cache',
-            },
-          },
-          {
-            loader: 'eslint-loader',
-            options: {
-              failOnError: !isDevelopment,
-              // emit warnings instead of errors to ensure the build
-              // succeeds even when there are eslint errors.
-              // (otherwise webpack devserver would refuse to reload,
-              // which is super-annoying during development.)
-              emitWarning: isDevelopment,
-              // emit errors instead of warnings to ensure the build
-              // fails even when there are only eslint warnings.
-              emitError: !isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
+module.exports = (env = {}) => {
+  return {
+    context: path.join(__dirname, 'src'),
+    entry: {
+      index: './index.js',
+      scrivito_extensions: './scrivito_extensions.js',
+      'index.css': './assets/stylesheets/index.scss',
+      danger_zone: './danger_zone.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js(\.jsx)?$/,
+          include: path.join(__dirname, 'src'),
           use: [
             {
-              loader: 'css-loader',
+              loader: 'babel-loader',
               options: {
-                minimize: !isDevelopment,
+                presets: ['react', 'es2015', 'es2016', 'es2017'],
+                plugins: ['transform-object-rest-spread', 'array-includes'],
+                cacheDirectory: 'tmp/babel-cache',
               },
-            }, {
-              loader: 'sass-loader',
+            },
+            {
+              loader: 'eslint-loader',
+              options: {
+                emitWarning: !env.production,
+              },
             },
           ],
-        }),
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'build'),
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: '../static' },
-      { from: '../vendor/scrivito', to: 'scrivito' },
-    ]),
-    new ExtractTextPlugin({
-      filename: '[name]',
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
-  resolve: {
-    alias: {
-      scrivito_sdk: path.join(__dirname, 'vendor/scrivito/scrivito_sdk.js'),
-    },
-    extensions: ['.js', '.js.jsx'],
-    modules: ['src', 'node_modules'],
-  },
-  devServer: {
-    // host: '0.0.0.0',
-    // disableHostCheck: true,
-    port: 8080,
-    historyApiFallback: {
-      rewrites: [
-        { from: /^\/scrivito/, to: '/scrivito/index.html' },
-        { from: /./, to: '/index.html' },
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: env.production,
+                },
+              }, {
+                loader: 'sass-loader',
+              },
+            ],
+          }),
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|gif)$/,
+          use: [
+            'file-loader',
+          ],
+        },
       ],
     },
-  },
+    output: {
+      filename: '[name].js',
+      path: path.join(__dirname, 'build'),
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: '../static' },
+        { from: '../vendor/scrivito', to: 'scrivito' },
+      ]),
+      new ExtractTextPlugin({
+        filename: '[name]',
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+    ],
+    resolve: {
+      alias: {
+        scrivito_sdk: path.join(__dirname, 'vendor/scrivito/scrivito_sdk.js'),
+      },
+      extensions: ['.js', '.js.jsx'],
+      modules: ['src', 'node_modules'],
+    },
+    devServer: {
+      // host: '0.0.0.0',
+      // disableHostCheck: true,
+      port: 8080,
+      historyApiFallback: {
+        rewrites: [
+          { from: /^\/scrivito/, to: '/scrivito/index.html' },
+          { from: /./, to: '/index.html' },
+        ],
+      },
+    },
+  };
 };
