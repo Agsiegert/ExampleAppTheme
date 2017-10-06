5e58d94161e971c7687a064040f6dca542087525
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-06 14:32:01 +0200

    Navigation: Search box navigates to search results page and closes search.

--- src/components/navigation/search.js.jsx
+++ src/components/navigation/search.js.jsx
@@ -1,4 +1,11 @@
 const SearchBox = Scrivito.connect(class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: '',
+    };
+  }
+
   componentDidUpdate() {
     this.initFocus();
   }
@@ -6,23 +13,28 @@ const SearchBox = Scrivito.connect(class extends React.Component {
   render() {
     return (
       <div className="search-box">
-        <form>
+        <form onSubmit={ e => this.handleSubmit(e) } >
           <div className="input-group">
             <input
               className="form-control"
               placeholder="Search..."
+              value={ this.state.q }
+              onChange={ e => this.onChange(e) }
               ref={ input => { this.input = input; } }
             />
             <div className="input-group-btn">
-              <button type="button" className="btn btn-search">
-                <i className="fa fa-search" aria-hidden="true"></i>
+              <button
+                type="submit"
+                className="btn btn-search"
+              >
+                <i className="fa fa-search" aria-hidden="true" />
               </button>
               <button
                 type="button"
                 className="btn btn-clear-white"
                 onClick={ this.props.toggleSearch }
               >
-                <i className="fa fa-times-circle " aria-hidden="true"></i>
+                <i className="fa fa-times-circle " aria-hidden="true" />
               </button>
             </div>
           </div>
@@ -35,6 +47,19 @@ const SearchBox = Scrivito.connect(class extends React.Component {
     if (this.props.showSearch) {
       this.input.focus();
     }
+  }
+
+  onChange(event) {
+    this.setState({ q: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const newParams = { q: this.state.q };
+    Scrivito.navigateTo(() => Scrivito.Obj.getByPermalink('search_results'), newParams);
+    this.setState({ q: '' });
+    this.props.toggleSearch();
   }
 });
 
