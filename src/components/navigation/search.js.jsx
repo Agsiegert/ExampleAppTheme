cdff56587b7d409c6daa745fa8fab998f6a75697
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-06 10:46:31 +0200

    NavigationSearch: switched to React.Component

--- src/components/navigation/search.js.jsx
+++ src/components/navigation/search.js.jsx
@@ -1,22 +1,28 @@
-function SearchBox({ toggleSearch }) {
-  return (
-    <div className="search-box">
-      <form>
-        <div className="input-group">
-          <input className="form-control" placeholder="Search..." />
-          <div className="input-group-btn">
-            <button type="button" className="btn btn-search">
-              <i className="fa fa-search" aria-hidden="true"></i>
-            </button>
-            <button type="button" className="btn btn-clear-white" onClick={ toggleSearch }>
-              <i className="fa fa-times-circle " aria-hidden="true"></i>
-            </button>
+const SearchBox = Scrivito.connect(class extends React.Component {
+  render() {
+    return (
+      <div className="search-box">
+        <form>
+          <div className="input-group">
+            <input className="form-control" placeholder="Search..." />
+            <div className="input-group-btn">
+              <button type="button" className="btn btn-search">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </button>
+              <button
+                type="button"
+                className="btn btn-clear-white"
+                onClick={ this.props.toggleSearch }
+              >
+                <i className="fa fa-times-circle " aria-hidden="true"></i>
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
-  );
-}
+        </form>
+      </div>
+    );
+  }
+});
 
 function SearchIcon({ toggleSearch }) {
   return (
