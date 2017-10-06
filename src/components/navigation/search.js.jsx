f6940c47ead7d1831943abc405a43851025d4064
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-06 08:42:43 +0000

    Added missing <form> tags to Search, Contact and Login form.

--- src/components/navigation/search.js.jsx
+++ src/components/navigation/search.js.jsx
@@ -1,17 +1,19 @@
 function SearchBox({ toggleSearch }) {
   return (
     <div className="search-box">
-      <div className="input-group">
-        <input className="form-control" placeholder="Search..." />
-        <div className="input-group-btn">
-          <button type="button" className="btn btn-search">
-            <i className="fa fa-search" aria-hidden="true"></i>
-          </button>
-          <button type="button" className="btn btn-clear-white" onClick={ toggleSearch }>
-            <i className="fa fa-times-circle " aria-hidden="true"></i>
-          </button>
+      <form>
+        <div className="input-group">
+          <input className="form-control" placeholder="Search..." />
+          <div className="input-group-btn">
+            <button type="button" className="btn btn-search">
+              <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+            <button type="button" className="btn btn-clear-white" onClick={ toggleSearch }>
+              <i className="fa fa-times-circle " aria-hidden="true"></i>
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
