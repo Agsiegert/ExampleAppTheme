a2236493e387cf6ed0dc7e694fe560d31ad4a2d8
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-09 13:47:28 +0200

    SearchResults: Extracted SearchInput into its own file.

--- src/objs/search_results.js.jsx
+++ src/objs/search_results.js.jsx
@@ -1,3 +1,4 @@
+import SearchInput from './search_results/search_input';
 import SearchResultItem from './search_results/search_result_item';
 
 const SearchResults = Scrivito.createObjClass({
@@ -17,62 +18,6 @@ Scrivito.provideEditingConfig(SearchResults, {
     },
   },
 });
-
-class SearchInput extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      q: props.params.q,
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.params.q !== nextProps.params.q) {
-      this.setState({ q: nextProps.params.q });
-    }
-  }
-
-  render() {
-    return (
-      <section className="bg-nav-content">
-        <div className="container">
-          <div className="nav-centered">
-            <form onSubmit={ e => this.handleSubmit(e) } >
-              <div className="input-group">
-                <input
-                  className="form-control"
-                  placeholder="Search for..."
-                  value={ this.state.q }
-                  onChange={ e => this.handleChange(e) }
-                />
-                <span className="input-group-btn">
-                  <button
-                    className="btn btn-primary"
-                    type="submit"
-                  >
-                    <span className="hidden-xs">Search again</span>
-                    <i className="fa fa-search fa-1" aria-hidden="true" />
-                  </button>
-                </span>
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
-    );
-  }
-
-  handleChange(event) {
-    this.setState({ q: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    Scrivito.navigateTo(() => Scrivito.currentPage(), { q: this.state.q });
-  }
-}
 
 class SearchResultsComponent extends React.Component {
   render() {
