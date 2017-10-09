5b97b658e52a495af020ce8fd5ae2abbc53d6737
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-09 11:47:50 +0200

    SearchResults: Show preview image if available.
    Also make headline clickable.

--- src/objs/search_results/search_result_item.js.jsx
+++ src/objs/search_results/search_result_item.js.jsx
@@ -2,6 +2,17 @@ import Highlighter from 'react-highlight-words';
 import textExtractFromObj from 'utils/text_extract_from_obj';
 import truncate from 'lodash.truncate';
 
+const PreviewImage = Scrivito.connect(({ item }) => {
+  const image = item.get('navigationBackgroundImage') || item.get('titleImage');
+  if (!image) { return null; }
+
+  return (
+    <Scrivito.LinkTag to={ item } className="result-image">
+      <Scrivito.ImageTag content={ image } />
+    </Scrivito.LinkTag>
+  );
+});
+
 function SearchResultItem({ resultItem, q }) {
   const searchWords = q.split(/\s+/);
   const textExtract = textExtractFromObj(resultItem);
@@ -9,8 +20,11 @@ function SearchResultItem({ resultItem, q }) {
 
   return (
     <div className="result-item">
+      <PreviewImage item={ resultItem } />
       <div className="result-content">
-        <h3 className="h3">{ resultItem.get('title') }</h3>
+        <Scrivito.LinkTag to={ resultItem }>
+          <h3 className="h3">{ resultItem.get('title') }</h3>
+        </Scrivito.LinkTag>
         <Highlighter
           highlightTag="mark"
           searchWords={ searchWords }
