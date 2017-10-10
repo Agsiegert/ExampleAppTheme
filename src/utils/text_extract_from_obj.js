1dd31b0f55f61d1ed33353eb3458c3e1281c3775
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-10 15:00:13 +0200

    textExtractFromObj now uses TextExtractRegistry instead of textExtract method.

--- src/utils/text_extract_from_obj.js
+++ src/utils/text_extract_from_obj.js
@@ -1,7 +1,51 @@
-function textExtractFromObj(obj) {
-  if (obj.textExtract) { return obj.textExtract(); }
+/* eslint no-console: 0 */
 
-  return '';
+import { decode } from 'ent';
+import striptags from 'striptags';
+import { lookupTextExtract } from 'utils/text_extract_registry';
+
+function textExtractFromObj(obj) {
+  return textExtractFromItem(obj);
+}
+
+function textExtractFromItem(objOrWidget) {
+  const attributes = lookupTextExtract(objOrWidget.objClass());
+
+  if (!attributes || !attributes.length) {
+    return '';
+  }
+
+  const textExtractValues = attributes.map(({ attribute, type }) => {
+    const value = objOrWidget.get(attribute);
+
+    switch (type) {
+      case 'html': return textExtractFromHtml(value);
+      case 'string': return value;
+      case 'widgetlist': return textExtractFromWidgetlist(value);
+      default: {
+        console.warn('[textExtractFromObj] type is not (yet?) support:', type);
+        return '';
+      }
+    }
+  });
+
+  return arrayToString(textExtractValues);
+}
+
+function textExtractFromHtml(html) {
+  return decode(striptags(html));
+}
+
+function textExtractFromWidgetlist(widgetlist) {
+  const textExtractValues = widgetlist.map(widget => textExtractFromItem(widget));
+  return arrayToString(textExtractValues);
+}
+
+function arrayToString(array) {
+  return array
+    .map(value => value.trim())
+    .filter(value => value)
+    .join('\n');
 }
 
 export default textExtractFromObj;
