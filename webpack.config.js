a0c0fa1d6913ce3d619e1684cb0dd09bc97484eb
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-16 10:59:01 +0200

    Use "dotenv" to read out ".env" file with a tenant ID.
    Also added a failure, if SCRIVITO_TENANT is not defined (fail early).

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const process = require('process');
+const dotenv = require('dotenv');
 const path = require('path');
+const process = require('process');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -7,11 +8,21 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const InlineEnvironmentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+// load ".env"
+dotenv.config();
+
 const buildPath = 'build';
 
 module.exports = (env = {}) => {
   // see https://github.com/webpack/webpack/issues/2537 for details
   const isProduction = process.argv.indexOf('-p') !== -1 || env.production;
+
+  if (!process.env.SCRIVITO_TENANT || process.env.SCRIVITO_TENANT === 'your_scrivito_tenant_id') {
+    throw('Environment variable "SCRIVITO_TENANT" is not defined!' +
+      ' Check if the ".env" file with a proper SCRIVITO_TENANT is set.' +
+      ' See ".env.example" for an example.'
+    );
+  }
 
   const plugins = [
     new webpack.DefinePlugin({
