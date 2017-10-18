b850851c6edcd5cb073aace17f9866d966751c6f
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-18 13:21:35 +0000

    AddressWidget: Improve order of attributes.

--- src/widgets/address_widget.js.jsx
+++ src/widgets/address_widget.js.jsx
@@ -21,23 +21,29 @@ Scrivito.provideEditingConfig(AddressWidget, {
   title: 'Address',
   description: 'A widget, which shows an address.',
   attributesConfig: {
-    showLogo: {
-      title: 'Show Logo',
-      description: 'Should the logo be shown? Default: yes',
-    },
     address: {
       title: 'Address',
       description: 'The actual address.',
-    },
-    showBorderBottom: {
-      title: 'Border Bottom',
-      description: 'Should a border be shown at the bottom? Default: no.',
     },
     listItems: {
       title: 'Address List Items',
       description: 'E.g. phone numbers.',
     },
+    showBorderBottom: {
+      title: 'Border Bottom',
+      description: 'Should a border be shown at the bottom? Default: no.',
+    },
+    showLogo: {
+      title: 'Show Logo',
+      description: 'Should the logo be shown? Default: yes',
+    },
   },
+  generalProperties: [
+    'showLogo',
+    'address',
+    'listItems',
+    'showBorderBottom',
+  ],
 });
 
 const AddMoreItems = Scrivito.connect(({ widget }) => {
