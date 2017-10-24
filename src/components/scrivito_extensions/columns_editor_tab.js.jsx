b56ec186fa57db9c850661bb963da5d43a9d75b5
Alexander Pepper <alexander.pepper@infopark.de>
2017-10-24 10:48:55 +0200

    ColumnsEditorTab is now a react component

--- src/components/scrivito_extensions/columns_editor_tab.js.jsx
+++ src/components/scrivito_extensions/columns_editor_tab.js.jsx
@@ -3,41 +3,51 @@ import isEqual from 'lodash/isEqual';
 import last from 'lodash/last';
 import times from 'lodash/times';
 
-Scrivito.registerComponent('ColumnsEditorTab', ({ widget }) =>
-  <div className="scrivito_detail_content">
-    <div className="scrivito_detail_label">
-      <span className="headline">Column Layout</span>
-    </div>
-    <div className="item_content">
-      <div className="gle-preview-list">
-        <div className="gle-preview-group">
-          <PresetGrid widget={ widget } title="1 column" grid={ [12] } />
+class ColumnsEditorTab extends React.Component {
+  render() {
+    return (
+      <div className="scrivito_detail_content">
+        <div className="scrivito_detail_label">
+          <span className="headline">Column Layout</span>
         </div>
-        <div className="gle-preview-group">
-          <PresetGrid widget={ widget } title="2 columns" grid={ [6, 6] } />
-          <PresetGrid widget={ widget } title="2 columns" grid={ [3, 9] } />
-          <PresetGrid widget={ widget } title="2 columns" grid={ [9, 3] } />
-        </div>
-        <div className="gle-preview-group">
-          <PresetGrid widget={ widget } title="3 columns" grid={ [4, 4, 4] } />
-          <PresetGrid widget={ widget } title="3 columns" grid={ [2, 8, 2] } />
-          <PresetGrid widget={ widget } title="3 columns" grid={ [2, 5, 5] } />
-          <PresetGrid widget={ widget } title="3 columns" grid={ [5, 5, 2] } />
-        </div>
-        <div className="gle-preview-group">
-          <PresetGrid widget={ widget } title="4 columns" grid={ [3, 3, 3, 3] } />
-          <PresetGrid widget={ widget } title="4 columns" grid={ [2, 4, 4, 2] } />
-        </div>
-        <div className="gle-preview-group">
-          <PresetGrid widget={ widget } title="5 columns" grid={ [2, 2, 2, 2, 4] } />
-        </div>
-        <div className="gle-preview-group">
-          <PresetGrid widget={ widget } title="6 columns" grid={ [2, 2, 2, 2, 2, 2] } />
+        <div className="item_content">
+          <div className="gle-preview-list">
+            <div className="gle-preview-group">
+              <PresetGrid widget={ this.props.widget } title="1 column" grid={ [12] } />
+            </div>
+            <div className="gle-preview-group">
+              <PresetGrid widget={ this.props.widget } title="2 columns" grid={ [6, 6] } />
+              <PresetGrid widget={ this.props.widget } title="2 columns" grid={ [3, 9] } />
+              <PresetGrid widget={ this.props.widget } title="2 columns" grid={ [9, 3] } />
+            </div>
+            <div className="gle-preview-group">
+              <PresetGrid widget={ this.props.widget } title="3 columns" grid={ [4, 4, 4] } />
+              <PresetGrid widget={ this.props.widget } title="3 columns" grid={ [2, 8, 2] } />
+              <PresetGrid widget={ this.props.widget } title="3 columns" grid={ [2, 5, 5] } />
+              <PresetGrid widget={ this.props.widget } title="3 columns" grid={ [5, 5, 2] } />
+            </div>
+            <div className="gle-preview-group">
+              <PresetGrid widget={ this.props.widget } title="4 columns" grid={ [3, 3, 3, 3] } />
+              <PresetGrid widget={ this.props.widget } title="4 columns" grid={ [2, 4, 4, 2] } />
+            </div>
+            <div className="gle-preview-group">
+              <PresetGrid widget={ this.props.widget } title="5 columns" grid={ [2, 2, 2, 2, 4] } />
+            </div>
+            <div className="gle-preview-group">
+              <PresetGrid
+                widget={ this.props.widget }
+                title="6 columns"
+                grid={ [2, 2, 2, 2, 2, 2] }
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+Scrivito.registerComponent('ColumnsEditorTab', ColumnsEditorTab);
 
 const PresetGrid = Scrivito.connect(({ widget, title, grid }) => {
   const classNames = ['gle-preview'];
