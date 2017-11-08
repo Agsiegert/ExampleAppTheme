b0c5dcbad804fa5fa578fe38e6865b3930324a54
Alexander Pepper <alexander.pepper@infopark.de>
2017-11-08 15:10:53 +0100

    icon.js.jsx => Icon.js

--- src/Widgets/IconListWidget/IconListWidgetComponent.js
+++ src/Widgets/IconListWidget/IconListWidgetComponent.js
@@ -1,4 +1,4 @@
-import IconComponent from 'Components/icon';
+import IconComponent from 'Components/Icon';
 
 Scrivito.provideComponent('IconListWidget', ({ widget }) =>
   <div className="social-links text-center">
