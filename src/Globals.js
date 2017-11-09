e9009b1c87a7107e80b3f25b0b2aab38e6532d31
Alexander Pepper <alexander.pepper@infopark.de>
2017-11-09 19:47:12 +0100

    Added PropTypes.
    They will soon be needed by the SDK anyway.

--- src/Globals.js
+++ src/Globals.js
@@ -1,6 +1,8 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+window.PropTypes = PropTypes;
 window.React = React;
 window.ReactDOM = ReactDOM;
 
