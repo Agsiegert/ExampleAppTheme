b80d60b5e12f02c9d374f95d9be99474d3442288
Alexander Pepper <alexander.pepper@infopark.de>
2017-11-22 13:49:13 +0100

    IconContainerWidget: Show a descriptive text, when no icons are selected.

--- src/Widgets/IconContainerWidget/IconContainerWidgetComponent.js
+++ src/Widgets/IconContainerWidget/IconContainerWidgetComponent.js
@@ -1,20 +1,33 @@
 import IconComponent from 'Components/Icon';
+import InPlaceEditingPlaceholder from 'Components/InPlaceEditingPlaceholder';
 
-Scrivito.provideComponent('IconContainerWidget', ({ widget }) =>
-  <div className="social-links text-center">
-    {
-      widget.get('iconList').map(iconListItem => {
-        const icon = iconListItem.get('icon');
-        const link = iconListItem.get('link');
-        const size = iconListItem.get('size');
+Scrivito.provideComponent('IconContainerWidget', ({ widget }) => {
+  const icons = widget.get('iconList');
 
-        return <IconComponent
-          icon={ icon }
-          size={ size }
-          link={ link }
-          key={ iconListItem.id() }
-        />;
-      })
-    }
-  </div>
-);
+  if (!icons.length) {
+    return (
+      <InPlaceEditingPlaceholder center={ true }>
+        No icons selected. Select them in the widget properties.
+      </InPlaceEditingPlaceholder>
+    );
+  }
+
+  return (
+    <div className="social-links text-center">
+      {
+        icons.map(iconListItem => {
+          const icon = iconListItem.get('icon');
+          const link = iconListItem.get('link');
+          const size = iconListItem.get('size');
+
+          return <IconComponent
+            icon={ icon }
+            size={ size }
+            link={ link }
+            key={ iconListItem.id() }
+          />;
+        })
+      }
+    </div>
+  );
+});
