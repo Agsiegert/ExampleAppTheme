67cf0726c08dfdfa77775d43911124e05d7f56e3
Alexander Pepper <alexander.pepper@infopark.de>
2017-11-22 13:05:42 +0100

    Refactored EventOverviewWidget

--- src/Widgets/EventOverviewWidget/EventOverviewWidgetComponent.js
+++ src/Widgets/EventOverviewWidget/EventOverviewWidgetComponent.js
@@ -3,45 +3,6 @@ import fullWidthTransformedUrl from 'utils/fullWidthTransformedUrl';
 import TagList from 'Components/TagList';
 import twoDigitNumber from 'utils/twoDigitNumber';
 
-function formatDate(date) {
-  if (!date) { return null; }
-
-  const month = date.getMonth() + 1; // getMonth return 0 to 11.
-  const dayOfMonth = date.getDate(); // getDate returns 1 to 31.
-
-  return `${twoDigitNumber(month)}/${twoDigitNumber(dayOfMonth)}`;
-}
-
-const EventItem = Scrivito.connect(({ event }) =>
-  <div className="col-sm-6">
-    <Scrivito.LinkTag
-      to={ event }
-      className="box-card"
-      style={ {
-        background: 'no-repeat center / cover',
-        backgroundImage: `linear-gradient(rgba(46, 53, 60, 0.7),
-          rgba(46, 53, 60, 0.7)),
-          url(${fullWidthTransformedUrl(event.get('image'))})`,
-      } }
-    >
-      <span className="box-date">
-        { formatDate(event.get('date')) }
-      </span>
-      <span className="box-topic dark-background">
-        <h3 className="h3">{ event.get('title') }</h3>
-        <span>
-          <i
-            className={ `fa ${event.get('location') ? 'fa-map-marker' : ''} fa-2x` }
-            aria-hidden="true"
-            title="location"
-          />
-          <span>{ event.get('location') }</span>
-        </span>
-      </span>
-    </Scrivito.LinkTag>
-  </div>
-);
-
 class EventOverviewWidgetComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -97,3 +58,42 @@ class EventOverviewWidgetComponent extends React.Component {
 }
 
 Scrivito.provideComponent('EventOverviewWidget', EventOverviewWidgetComponent);
+
+const EventItem = Scrivito.connect(({ event }) =>
+  <div className="col-sm-6">
+    <Scrivito.LinkTag
+      to={ event }
+      className="box-card"
+      style={ {
+        background: 'no-repeat center / cover',
+        backgroundImage: `linear-gradient(rgba(46, 53, 60, 0.7),
+          rgba(46, 53, 60, 0.7)),
+          url(${fullWidthTransformedUrl(event.get('image'))})`,
+      } }
+    >
+      <span className="box-date">
+        { formatDate(event.get('date')) }
+      </span>
+      <span className="box-topic dark-background">
+        <h3 className="h3">{ event.get('title') }</h3>
+        <span>
+          <i
+            className={ `fa ${event.get('location') ? 'fa-map-marker' : ''} fa-2x` }
+            aria-hidden="true"
+            title="location"
+          />
+          <span>{ event.get('location') }</span>
+        </span>
+      </span>
+    </Scrivito.LinkTag>
+  </div>
+);
+
+function formatDate(date) {
+  if (!date) { return null; }
+
+  const month = date.getMonth() + 1; // getMonth return 0 to 11.
+  const dayOfMonth = date.getDate(); // getDate returns 1 to 31.
+
+  return `${twoDigitNumber(month)}/${twoDigitNumber(dayOfMonth)}`;
+}
