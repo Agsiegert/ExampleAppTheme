968a378e75c2ba5332528177375aea0e573a5dbd
Alexander Pepper <alexander.pepper@infopark.de>
2017-11-23 10:34:44 +0100

    VideoWidget: Improved placeholder message for empty videos.

--- src/Widgets/VideoWidget/VideoWidgetComponent.js
+++ src/Widgets/VideoWidget/VideoWidgetComponent.js
@@ -1,19 +1,15 @@
+import InPlaceEditingPlaceholder from 'Components/InPlaceEditingPlaceholder';
 import urlFromBinary from 'utils/urlFromBinary';
 
 Scrivito.provideComponent('VideoWidget', ({ widget }) => {
   const videoUrl = urlFromBinary(widget.get('source'));
 
   if (!videoUrl) {
-    if (Scrivito.isInPlaceEditingActive()) {
-      return (
-        <p>
-          No Video (yet)!
-          Please go to the widget properties to select or upload a video.
-        </p>
-      );
-    }
-
-    return null;
+    return (
+      <InPlaceEditingPlaceholder center={ true }>
+        No video selected. Select or upload one in the widget properties.
+      </InPlaceEditingPlaceholder>
+    );
   }
 
   const posterUrl = urlFromBinary(widget.get('poster'));
