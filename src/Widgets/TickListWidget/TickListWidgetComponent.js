5c165c694d86bc729b621c00c15676904ebdee88
Alexander Pepper <alexander.pepper@infopark.de>
2017-11-23 09:32:19 +0000

    TickListWidget: Show a descriptive text, when no statements are provided.

--- src/Widgets/TickListWidget/TickListWidgetComponent.js
+++ src/Widgets/TickListWidget/TickListWidgetComponent.js
@@ -1,14 +1,28 @@
-Scrivito.provideComponent('TickListWidget', ({ widget }) =>
-  <ul className="tick-list">
-    {
-      widget.get('items').map((itemWidget, index) =>
-        <Scrivito.ContentTag
-          key={ `${itemWidget.id()}${index}` }
-          content={ itemWidget }
-          attribute="statement"
-          tag="li"
-        />
-      )
-    }
-  </ul>
-);
+import InPlaceEditingPlaceholder from 'Components/InPlaceEditingPlaceholder';
+
+Scrivito.provideComponent('TickListWidget', ({ widget }) => {
+  const items = widget.get('items');
+
+  if (!items.length) {
+    return (
+      <InPlaceEditingPlaceholder>
+        No statements provided. Provide them in the widget properties.
+      </InPlaceEditingPlaceholder>
+    );
+  }
+
+  return (
+    <ul className="tick-list">
+      {
+        items.map((itemWidget, index) =>
+          <Scrivito.ContentTag
+            key={ `${itemWidget.id()}${index}` }
+            content={ itemWidget }
+            attribute="statement"
+            tag="li"
+          />
+        )
+      }
+    </ul>
+  );
+});
