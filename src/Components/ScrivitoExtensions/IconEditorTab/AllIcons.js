8e75872777a05626b5c57b979f21d98be338f050
Alexander Pepper <alexander.pepper@infopark.de>
2017-12-04 12:19:53 +0100

    Icon Editor: Calculate categories and search fuse in constructor, not during js load.

--- src/Components/ScrivitoExtensions/IconEditorTab/AllIcons.js
+++ src/Components/ScrivitoExtensions/IconEditorTab/AllIcons.js
@@ -1,46 +1,54 @@
 import fontAwesomeIcons from './fontAwesomeIcons';
 import SingleIcon from './SingleIcon';
 
-function AllIcons({ setWidgetIcon, currentIcon, hide }) {
-  if (hide) { return null; }
+class AllIcons extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <React.Fragment>
-      <div className="scrivito_detail_label">
-        <span>All icons</span>
-      </div>
-      <div id="icons">
-        {
-          Object.entries(categoryMap).map(([category, icons], index) =>
-            <section key={ `${category}${index}` }>
-              <i>{ category }</i>
-              <div className="row">
-                {
-                  icons.map((icon, innerIndex) =>
-                    <SingleIcon
-                      key={ `${icon.id}${innerIndex}` }
-                      icon={ icon }
-                      currentIcon={ currentIcon }
-                      setWidgetIcon={ setWidgetIcon }
-                    />)
-                }
-              </div>
-            </section>
-          )
+    this.categoryMap = {};
+    fontAwesomeIcons.forEach(
+      icon => icon.categories.forEach(
+        category => {
+          if (!this.categoryMap[category]) { this.categoryMap[category] = []; }
+          this.categoryMap[category].push(icon);
         }
-      </div>
-    </React.Fragment>
-  );
-}
+      )
+    );
+  }
 
-const categoryMap = {};
-fontAwesomeIcons.forEach(
-  icon => icon.categories.forEach(
-    category => {
-      if (!categoryMap[category]) { categoryMap[category] = []; }
-      categoryMap[category].push(icon);
-    }
-  )
-);
+  render() {
+    const { setWidgetIcon, currentIcon, hide } = this.props;
+
+    if (hide) { return null; }
+
+    return (
+      <React.Fragment>
+        <div className="scrivito_detail_label">
+          <span>All icons</span>
+        </div>
+        <div id="icons">
+          {
+            Object.entries(this.categoryMap).map(([category, icons], index) =>
+              <section key={ `${category}${index}` }>
+                <i>{ category }</i>
+                <div className="row">
+                  {
+                    icons.map((icon, innerIndex) =>
+                      <SingleIcon
+                        key={ `${icon.id}${innerIndex}` }
+                        icon={ icon }
+                        currentIcon={ currentIcon }
+                        setWidgetIcon={ setWidgetIcon }
+                      />)
+                  }
+                </div>
+              </section>
+            )
+          }
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default AllIcons;
--- src/Components/ScrivitoExtensions/IconEditorTab/IconSearchResults.js
+++ src/Components/ScrivitoExtensions/IconEditorTab/IconSearchResults.js
@@ -2,51 +2,59 @@ import Fuse from 'fuse.js';
 import fontAwesomeIcons from './fontAwesomeIcons';
 import SingleIcon from './SingleIcon';
 
-function IconSearchResults({ searchValue, setWidgetIcon, currentIcon }) {
-  if (!searchValue.length) { return null; }
+class IconSearchResults extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const results = fuse.search(searchValue);
+    const fuseOptions = {
+      shouldSort: true,
+      includeMatches: true,
+      threshold: 0.2,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 35,
+      minMatchCharLength: 1,
+      keys: [
+        'name',
+        'id',
+        'filter',
+        'aliases',
+      ],
+    };
+    this.fuse = new Fuse(fontAwesomeIcons, fuseOptions);
+  }
 
-  return (
-    <div id="search-results">
-      <div className="scrivito_detail_label">
-        <span>
-          { `Search for '${searchValue}'` }
-        </span>
+  render() {
+    const { searchValue, setWidgetIcon, currentIcon } = this.props;
+
+    if (!searchValue.length) { return null; }
+
+    const results = this.fuse.search(searchValue);
+
+    return (
+      <div id="search-results">
+        <div className="scrivito_detail_label">
+          <span>
+            { `Search for '${searchValue}'` }
+          </span>
+        </div>
+        <div className="row">
+          {
+            results.map((result, index) => {
+              const icon = result.item;
+
+              return <SingleIcon
+                key={ `${icon.id}${index}` }
+                icon={ icon }
+                currentIcon={ currentIcon }
+                setWidgetIcon={ setWidgetIcon }
+              />;
+            })
+          }
+        </div>
       </div>
-      <div className="row">
-        {
-          results.map((result, index) => {
-            const icon = result.item;
-
-            return <SingleIcon
-              key={ `${icon.id}${index}` }
-              icon={ icon }
-              currentIcon={ currentIcon }
-              setWidgetIcon={ setWidgetIcon }
-            />;
-          })
-        }
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-const fuseOptions = {
-  shouldSort: true,
-  includeMatches: true,
-  threshold: 0.2,
-  location: 0,
-  distance: 100,
-  maxPatternLength: 35,
-  minMatchCharLength: 1,
-  keys: [
-    'name',
-    'id',
-    'filter',
-    'aliases',
-  ],
-};
-const fuse = new Fuse(fontAwesomeIcons, fuseOptions);
 
 export default IconSearchResults;
