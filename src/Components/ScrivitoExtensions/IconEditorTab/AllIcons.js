1b5adb3dd5c3fab7421e80e64114c2df827dceaf
Alexander Pepper <alexander.pepper@infopark.de>
2017-12-04 17:24:40 +0100

    AllIcons: Inlined SignleIcon to improve render speed.

--- src/Components/ScrivitoExtensions/IconEditorTab/AllIcons.js
+++ src/Components/ScrivitoExtensions/IconEditorTab/AllIcons.js
@@ -1,6 +1,5 @@
 import take from 'lodash/take';
 import fontAwesomeIcons from './fontAwesomeIcons';
-import SingleIcon from './SingleIcon';
 
 class AllIcons extends React.Component {
   constructor(props) {
@@ -87,13 +86,31 @@ function Category({ category, icons, currentIcon, setWidgetIcon }) {
       <i>{ category }</i>
       <div className="row">
         {
-          icons.map((icon, innerIndex) =>
-            <SingleIcon
-              key={ `${icon.id}${innerIndex}` }
-              icon={ icon }
-              currentIcon={ currentIcon }
-              setWidgetIcon={ setWidgetIcon }
-            />)
+          icons.map((icon, innerIndex) => {
+            const cssIcon = `fa-${icon.id}`;
+
+            const aClassNames = [];
+            if (currentIcon === cssIcon) {
+              aClassNames.push('active');
+            }
+
+            return (
+              <div
+                key={ `${icon.id}${innerIndex}` }
+                className="fa-hover col-md-3 col-sm-4"
+              >
+                <a
+                  href="#"
+                  className={ aClassNames.join(' ') }
+                  onClick={ e => setWidgetIcon(e, cssIcon) }
+                >
+                  <i className= { ['fa', cssIcon].join(' ') } aria-hidden="true" />
+                  <span className="sr-only">Example of </span>
+                  { icon.name }
+                </a>
+              </div>
+            );
+          })
         }
       </div>
     </section>
