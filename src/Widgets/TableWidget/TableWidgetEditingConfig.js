9cb27c8f089b7a4f6b159112a4414a0df499e8e6
Alexander Pepper <alexander.pepper@infopark.de>
2017-12-06 14:34:05 +0100

    Added initialContent for TableWidget.

--- src/Widgets/TableWidget/TableWidgetEditingConfig.js
+++ src/Widgets/TableWidget/TableWidgetEditingConfig.js
@@ -1,4 +1,7 @@
+import IconWidget from 'Widgets/IconWidget/IconWidgetClass';
+import TableRowWidget from 'Widgets/TableRowWidget/TableRowWidgetClass';
 import tableWidgetIcon from 'assets/images/table_widget.svg';
+import TextWidget from 'Widgets/TextWidget/TextWidgetClass';
 
 Scrivito.provideEditingConfig('TableWidget', {
   title: 'Table',
@@ -17,4 +20,38 @@ Scrivito.provideEditingConfig('TableWidget', {
     'currency',
     'rows',
   ],
+  initialContent: {
+    featureHeadline: 'Features',
+    basicPlanHeadline: 'Basic',
+    teamPlanHeadline: 'Team',
+    corporatePlanHeadline: 'Corporate',
+    pricingRow: 'Prices',
+    currency: '€',
+    basicPlanPrice: '20',
+    teamPlanPrice: '30',
+    corporatePlanPrice: '40',
+    basicPlanPeriod: '/mo',
+    teamPlanPeriod: '/mo',
+    corporatePlanPeriod: '/mo',
+    rows: () => [
+      new TableRowWidget({
+        feature: 'Key feature 1',
+        basicFeature: [new TextWidget({ text: '<p><b>5</b></p>' })],
+        teamFeature: [new TextWidget({ text: '<p><b>10</b></p>' })],
+        corporateFeature: [new TextWidget({ text: '<p><b>unlimited</b></p>' })],
+      }),
+      new TableRowWidget({
+        feature: 'Key feature 2',
+        basicFeature: [new TextWidget({ text: '<p><b>20</b></p>' })],
+        teamFeature: [new TextWidget({ text: '<p><b>40</b></p>' })],
+        corporateFeature: [new TextWidget({ text: '<p><b>unlimited</b></p>' })],
+      }),
+      new TableRowWidget({
+        feature: 'Key feature 3',
+        basicFeature: [new TextWidget({ text: '<p><b>-</b></p>' })],
+        teamFeature: [new IconWidget({ icon: 'fa-check' })],
+        corporateFeature: [new IconWidget({ icon: 'fa-check' })],
+      }),
+    ],
+  },
 });
